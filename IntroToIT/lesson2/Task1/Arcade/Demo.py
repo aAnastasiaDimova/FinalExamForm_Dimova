027d5298c8731055f4192a27e969958604d4d479
Elwin <elwinner23@yandex.ru>
2023-10-11 13:45:47 +0400

    DC heroes version of the game

--- IntroToIT/lesson2/Task1/Arcade/Demo.py
+++ IntroToIT/lesson2/Task1/Arcade/Demo.py
@@ -16,10 +16,10 @@ shift = 0
 x_start, y_start = 20, 10
 
 img_file_back =     path.dirname(__file__)+ '/cave.png'
-img_file_hero =     path.dirname(__file__)+ '/m1.png'
-img_file_enemy =    path.dirname(__file__)+ '/enemy.png' 
+img_file_hero =     path.dirname(__file__)+ '/superman.png'
+img_file_enemy =    path.dirname(__file__)+ '/bane.png' 
 img_file_bomb =     path.dirname(__file__)+ '/bomb.png'
-img_file_princess = path.dirname(__file__)+ '/princess.png'
+img_file_princess = path.dirname(__file__)+ '/batwoman.png'
 FPS = 60
 
 # цвета:
