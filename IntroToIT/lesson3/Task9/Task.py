3a4dae6b2563e00a3816b159c59bd9b2a0af2f30
= <=>
2023-10-17 19:27:04 +0500

    fix

--- IntroToIT/lesson3/Task9/Task.py
+++ IntroToIT/lesson3/Task9/Task.py
@@ -2,5 +2,5 @@
 n = int(input("Введите n: "))
 sum = 0
 for i in range(1, n+1):
-    sum - = i
-print("Сумма чисел от 1 до", n, "равна:", sum)
+    sum + = i
+print("Сумма чисел от 1 до n", "равна:", sum)
