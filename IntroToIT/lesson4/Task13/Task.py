15b291362532379adef0e898e977ea27d0afbcb9
Johnny Cage <johnnycage@example.com>
2023-10-25 20:50:50 +0300

    ^._.^

--- IntroToIT/lesson4/Task13/Task.py
+++ IntroToIT/lesson4/Task13/Task.py
@@ -2,8 +2,11 @@
 # Задача 13: Проверить, является ли список отсортированным. 
 
 # Неправильное решение:
-def wrong_is_sorted(lst):
+def right_is_sorted(lst):
     for i in range(len(lst) - 1):
         if lst[i] > lst[i + 1]:
             return True
     return False
+
+def right_is_sorted(lst):
+    return list.sorts
