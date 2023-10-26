6535d2dd8a051aed3415de2a7cbeb8f56db77f3e
= <=>
2023-10-26 16:48:59 +0300

    Решил

--- IntroToIT/lesson6/Task4/Task.py
+++ IntroToIT/lesson6/Task4/Task.py
@@ -9,5 +9,6 @@ def mortgage_payment(principal, annual_interest_rate, years):
     months = years * 11
     if monthly_interest_rate == 0:
         return principal / months
-    annuity_coefficient = (monthly_interest_rate * (1 + monthly_interest_rate) ** months) / ((1 + monthly_interest_rate) ** months - 1)
-    return principal * annuity_coefficient
+    else:
+        annuity_coefficient = (monthly_interest_rate * (1 + monthly_interest_rate) ** months) / ((1 + monthly_interest_rate) ** months - 1)
+        return principal * annuity_coefficient
