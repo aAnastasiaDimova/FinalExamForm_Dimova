432b0b74b5fe5679402e556bbfba6bb610faed26
game <_@example.com>
2023-10-27 09:10:36 -0700

    fix: ("исправил ошибки с помощью тестирования")

--- IntroToIT/lesson6/Task2/Task.py
+++ IntroToIT/lesson6/Task2/Task.py
@@ -3,11 +3,16 @@
 #(Пусть это будет функция, которая возвращает количество дней до дня рождения)
 from datetime import datetime
 
-def days_until_birthday(birZday):
+def days_until_birthday(birthday):
     today = datetime.today()
     next_birthday = datetime(today.year, birthday.month, birthday.day)
     if today > next_birthday:
-        next_birthday = datetime(today.year, birthday.month, birthday.day)
-    return (next_birthday - today).days + 1
+        next_birthday = datetime(today.year + 1, birthday.month, birthday.day)
+    days_until = (next_birthday - today).days
+    return days_until
 
- 
+today = datetime(2023, 10, 27)
+birthday = datetime(2024, 12, 31)
+
+days = (birthday - today).days
+print(f"До вашего дня рождения осталось {days} дней.")
--- IntroToIT/lesson6/Task7/Task.py
+++ IntroToIT/lesson6/Task7/Task.py
@@ -1,4 +1,5 @@
 #INTRO TO IT 2nd COURSE
 def heron_triangle_area(a, b, c):
     s = (a + b + c) / 2
-    return (s*(s-a)*(s-b)*(s-c)) * 0.5
+    area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+    return area
