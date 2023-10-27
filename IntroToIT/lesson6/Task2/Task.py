a30c51499b5fcde9f34bc8912169bba7781216c9
Vera <vringden@gmail.com>
2023-10-27 21:06:49 +0300

    12

--- IntroToIT/lesson6/Task2/Task.py
+++ IntroToIT/lesson6/Task2/Task.py
@@ -2,8 +2,7 @@
 #Рассчитывание дня рождения:
 #(Пусть это будет функция, которая возвращает количество дней до дня рождения)
 from datetime import datetime
-
-def days_until_birthday(birZday):
+def days_until_birthday(birthday):
     today = datetime.today()
     next_birthday = datetime(today.year, birthday.month, birthday.day)
     if today > next_birthday:
