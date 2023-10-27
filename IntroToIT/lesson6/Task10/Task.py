61240a9195b13ec4dde841255db43500d71b25dd
Ruslan Borisov <johndoe@example.com>
2023-10-27 20:54:50 +0300

    10 fix anyway

--- IntroToIT/lesson6/Task10/Task.py
+++ IntroToIT/lesson6/Task10/Task.py
@@ -1,7 +1,7 @@
 #INTRO TO IT 2nd COURSE
 #Задача 10: Вычисление факториала
-def fac(n):
+def factorial(n):
     if n == 0:
         return 1
     else:
-        return n * fac(n-1)
+        return n * factorial(n-1)
