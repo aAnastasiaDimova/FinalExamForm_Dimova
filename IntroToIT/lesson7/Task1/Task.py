9b5cbb5f379255d4b10e5c444900914dafceabb1
lisyonok14 <olesya.nikulina.2007@mail.ru>
2023-11-06 17:37:09 +0200

    fix: have been fixed 1-3 tasks

--- IntroToIT/lesson7/Task1/Task.py
+++ IntroToIT/lesson7/Task1/Task.py
@@ -1,21 +1,21 @@
 #INTRO TO IT 2nd COURSE
 def add_numbers(a, b):
-
+# Функция принимает 2 числа - a, b и возвращает их сумму
     result = a + b
     return result
 
 def multiply_numbers(a, b):
-
+# Функция принимает 2 числа - a, b и возвращает их произведение
     result = a * b
     return result
 
 def find_max_number(numbers):
-
+# Функция принимает числа - numbers и возвращает максимальное
     max_number = max(numbers)
     return max_number
 
 def calculate_factorial(n):
- 
+ # Функция принимает число - n и возвращает его факториал
     if n == 0:
         return 1
     factorial = 1
@@ -24,7 +24,8 @@ def calculate_factorial(n):
     return factorial
 
 def is_even(number):
-
+# Функция принимает число - number. Возвращает True если
+# число четное, False если нет
     if number % 2 == 0:
         return True
     else:
@@ -32,17 +33,27 @@ def is_even(number):
 
 num1 = 10
 num2 = 5
+# Переменные для функций
 sum_result = add_numbers(num1, num2)
+# В sum_result записываем результат функции add_numbers(num1, num2)
 product_result = multiply_numbers(num1, num2)
+# В product_result записываем результат функции multiply_numbers(num1, num2)
 numbers_list = [3, 8, 1, 6, 12]
+# Переменная со списком для функции
 max_num = find_max_number(numbers_list)
+# В max_num записываем результат функции find_max_number(numbers_list)
 factorial_result = calculate_factorial(5)
+# В factorial_result записываем результат функции calculate_factorial(5)
 is_even_num = is_even(7)
+# В is_even_num записываем результат функции is_even(7)
 
+# Выводим результаты
 print(f"Сумма чисел {num1} и {num2} равна {sum_result}")
 print(f"Произведение чисел {num1} и {num2} равно {product_result}")
 print(f"Наибольшее число в списке {numbers_list} - {max_num}")
 print(f"Факториал числа 5 равен {factorial_result}")
+
+# Выводим "Число 7 - нечетное." т.к. результат функции False
 if is_even_num:
     print("Число 7 - четное.")
 else:
---
+++ IntroToIT/lesson7/Task3/README.md
@@ -0,0 +1,27 @@
+# Shift.IntroToIT.Base
+---
+## Описание 
+---
+### 8 уроков с заданиями для введения в IT
+
+## Инструкция по установке
+---
+![Копируем ссылку на репозиторий](https://sun9-57.userapi.com/impg/HbbomnHqKE-1-FjrB3NugylySWZcRnCSUHKcSA/n2zWjerL2VE.jpg?size=1864x1132&quality=96&sign=d1c9524cba4fca64a572111adae525ed&type=album)
+- Открываете Visual Studio Code
+- Переходите в раздел Source Control (значок ветки) и нажимаете на Clone
+![-](https://sun9-65.userapi.com/impg/2vISE9KyKkvZiJNCj1aTrW_8SXqhiV3dzgharA/aYebbtvysG0.jpg?size=438x746&quality=96&sign=26ec28fb2fccc0c027da4de9abf723b7&type=album)
+- Сверху выбираете клонирование с GitHub и вставляете ссылку на репозиторий
+- Выбираете место сохранения
+
+## Инструкция по созданию ветки/использованию
+---
+- Снизу на значке ветки, где написано main нажимаете и выбираете dev_2nd_course
+- В Source Control нажимаем на 3 точки - Branch - Create Branch From...
+- Создаёте ветку со своим именем (dev_Petya) от ветки dev_2nd_course
+- Переключаетесь на созданную ветку
+- Опубликовываете ветку (push)
+- Работаете в уроках
+
+## Авторы
+---
+- alkihuri
