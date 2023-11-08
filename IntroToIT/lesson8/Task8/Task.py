ceb94e2732b63227318bf25e9c365fcb6ccf3d24
Надежда <kuznechik25092005@gmail.com>
2023-11-08 17:39:44 +0300

    fix: исправление ошибок

--- IntroToIT/lesson8/Task8/Task.py
+++ IntroToIT/lesson8/Task8/Task.py
@@ -3,4 +3,6 @@
 def print_numbers_skip_divisible(n, m):
     for i in range(1, n+1):
         if i % m == 0:
+            continue
+        else:
             print(i)  # неправильное размещение print
