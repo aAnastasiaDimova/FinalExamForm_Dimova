155cf1aca900f9a1d0f54fc055a1c5aea77849fb
Nikita Bykov <nikita.bykov.322@gmail.com>
2023-11-08 17:17:31 +0300

    fix: Исправлены баги в коде

--- IntroToIT/lesson8/Task5/Task.py
+++ IntroToIT/lesson8/Task5/Task.py
@@ -2,3 +2,5 @@
 # Задача: сгенерировать список всех четных чисел до N
 def generate_evens(n):
     return [i for i in range(n) if i % 2 == 0]  # интервал в генераторе списка неправильный
+
+print(generate_evens(100))
