1348b9f4d784807feb6963e0ad76da76a42d1fac
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2023-11-08 19:51:58 +0300

    docs: Добавил комментарии к коду и составил документацию к проеекту

--- IntroToIT/lesson7/Task1/Task.py
+++ IntroToIT/lesson7/Task1/Task.py
@@ -1,47 +1,48 @@
 #INTRO TO IT 2nd COURSE
+# Функция для вычесления суммы двух чисел
 def add_numbers(a, b):
-
     result = a + b
-    return result
+    return result  
 
+# Функция для вычисления произведения двух чисел
 def multiply_numbers(a, b):
-
     result = a * b
-    return result
+    return result  
 
+# Функция для вычисления максимального значения 
 def find_max_number(numbers):
-
     max_number = max(numbers)
-    return max_number
+    return max_number  
 
+# Функция для вычисления факториала
 def calculate_factorial(n):
- 
     if n == 0:
         return 1
     factorial = 1
     for i in range(1, n + 1):
         factorial *= i
-    return factorial
+    return factorial  
 
+# Функция для вычисления истинности четности числа
 def is_even(number):
-
     if number % 2 == 0:
         return True
     else:
-        return False
+        return False  
 
 num1 = 10
 num2 = 5
-sum_result = add_numbers(num1, num2)
-product_result = multiply_numbers(num1, num2)
-numbers_list = [3, 8, 1, 6, 12]
-max_num = find_max_number(numbers_list)
-factorial_result = calculate_factorial(5)
-is_even_num = is_even(7)
+sum_result = add_numbers(num1, num2) # Сумма двух переменных 
+product_result = multiply_numbers(num1, num2) # Произведение двух переменных 
+numbers_list = [3, 8, 1, 6, 12] 
+max_num = find_max_number(numbers_list) # Нахождение максимального значение спика 
+factorial_result = calculate_factorial(5) # Нахождение факториала списка
+is_even_num = is_even(7) # Провекра на четность 
 
+# Вывод значений 
 print(f"Сумма чисел {num1} и {num2} равна {sum_result}")
 print(f"Произведение чисел {num1} и {num2} равно {product_result}")
-print(f"Наибольшее число в списке {numbers_list} - {max_num}")
+print(f"Наибольшее число в списке {numbers_list} - {max_num}") 
 print(f"Факториал числа 5 равен {factorial_result}")
 if is_even_num:
     print("Число 7 - четное.")
