2f7dce3e4f56516513650996432ff76308340a38
= <huilovpisa@gmail.com>
2023-11-08 21:11:05 +0300

    fix исправил ошибки в lesson 7

--- IntroToIT/lesson7/Task1/Task.py
+++ IntroToIT/lesson7/Task1/Task.py
@@ -1,21 +1,21 @@
 #INTRO TO IT 2nd COURSE
+# Функция для сложения двух чисел
 def add_numbers(a, b):
-
     result = a + b
     return result
 
+# Функция для умножения двух чисел
 def multiply_numbers(a, b):
-
     result = a * b
     return result
 
+# Функция для поиска максимального числа в списке
 def find_max_number(numbers):
-
     max_number = max(numbers)
     return max_number
 
+# Функция для вычисления факториала числа
 def calculate_factorial(n):
- 
     if n == 0:
         return 1
     factorial = 1
@@ -23,22 +23,26 @@ def calculate_factorial(n):
         factorial *= i
     return factorial
 
+# Функция для проверки, является ли число четным
 def is_even(number):
-
     if number % 2 == 0:
         return True
     else:
         return False
 
+# Задание значений для переменных
 num1 = 10
 num2 = 5
+numbers_list = [3, 8, 1, 6, 12]
+
+# Вызов функций с различными входными данными
 sum_result = add_numbers(num1, num2)
 product_result = multiply_numbers(num1, num2)
-numbers_list = [3, 8, 1, 6, 12]
 max_num = find_max_number(numbers_list)
 factorial_result = calculate_factorial(5)
 is_even_num = is_even(7)
 
+# Вывод результатов на экран
 print(f"Сумма чисел {num1} и {num2} равна {sum_result}")
 print(f"Произведение чисел {num1} и {num2} равно {product_result}")
 print(f"Наибольшее число в списке {numbers_list} - {max_num}")
