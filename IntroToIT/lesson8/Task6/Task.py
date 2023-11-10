9b0b3f8cafd5dd1abfb55ff04e9032687c46aae3
Юрий <zaretskiy.y.a@gmail.com>
2023-11-10 18:52:03 +0300

    fix Task 1-2 and fix Task 4-10

--- IntroToIT/lesson8/Task6/Task.py
+++ IntroToIT/lesson8/Task6/Task.py
@@ -1,7 +1,5 @@
 #INTRO TO IT 2nd COURSE
 # Задача: вернуть строку в обратном порядке
 def reverse_string(s):
-    reversed_s = ''
-    for char in s:
-        reversed_s = reversed_s  # неправильное присваивание для реверсирования строки
-    return reversed_s
+    reversed_s = '' # неправильное присваивание для реверсирования строки
+    return reversed_s[::-1]
